67f1160c065625a9fb91e66f28ea9b99c6c9cb88
hayato94087 <hayato94087@gmail.com>
2025-04-22 18:45:36 +0900

    config を導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,11 +10,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const eslintConfig = tseslint.config(
+  ...compat.extends("next/core-web-vitals"),
+  {
+    extends: [...tseslint.configs.recommended],
+  },
   {
     ignores: ["**/.next/**", "**/node_modules/**"],
   },
-];
+);
 
 export default eslintConfig;
